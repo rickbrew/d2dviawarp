8efaef127d6f35d82e943fc587bd3eaa6efb3fe7
Rick Brewster <rickbrew@users.noreply.github.com>
2018-03-10 20:46:14 -0800

    argh
    still won't work

--- dxgi/dxgi.cpp
+++ dxgi/dxgi.cpp
@@ -12,13 +12,15 @@
 
 // DXGI
 #include <dxgi.h>
+#include <dxgi1_2.h>
+#pragma comment(lib, "dxgi.lib")
 
 // Direct3D
-//#include <d3d10_1.h>
-//#pragma comment(lib, "d3d10_1.lib")
+#include <d3d10_1.h>
+#pragma comment(lib, "d3d10_1.lib")
 
-#include <d3d11.h>
-#pragma comment(lib, "d3d11.lib")
+//#include <d3d11.h>
+//#pragma comment(lib, "d3d11.lib")
 
 // Direct2D
 #include <d2d1_2.h>
@@ -48,13 +50,19 @@ int main()
             reinterpret_cast<void**>(&spImagingFactory));
     }
 
+    CComPtr<IDXGIFactory1> spDxgiFactory;
+    if (SUCCEEDED(hr))
+    {
+        hr = CreateDXGIFactory1(IID_PPV_ARGS(&spDxgiFactory));
+    }
+
     CComPtr<ID2D1Factory2> spD2DFactory;
     if (SUCCEEDED(hr))
     {
         D2D1_FACTORY_OPTIONS options;
         ZeroMemory(&options, sizeof(options));
 
-        options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION; //D2D1_DEBUG_LEVEL_NONE;
+        options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
 
         hr = D2D1CreateFactory(
             D2D1_FACTORY_TYPE_MULTI_THREADED,
@@ -62,9 +70,31 @@ int main()
             &spD2DFactory);
     }
 
+    CComPtr<ID2D1Multithread> spD2DLock;
+    if (SUCCEEDED(hr))
+    {
+        hr = spD2DFactory->QueryInterface(&spD2DLock);
+    }
+
+    CComPtr<ID3D10Device1> spD3DDevice;
+    D3D_FEATURE_LEVEL d3dFeatureLevel = D3D_FEATURE_LEVEL_10_1;
+    if (SUCCEEDED(hr))
+    {
+        hr = D3D10CreateDevice1(
+            NULL,
+            D3D10_DRIVER_TYPE_WARP,
+            NULL,
+            D3D10_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS
+                | D3D10_CREATE_DEVICE_BGRA_SUPPORT
+                | D3D10_CREATE_DEVICE_DEBUG,
+            D3D10_FEATURE_LEVEL_10_1,
+            D3D10_1_SDK_VERSION,
+            &spD3DDevice);
+    }
+    
+    /*
     CComPtr<ID3D11Device> spD3DDevice;
     CComPtr<ID3D11DeviceContext> spD3DDeviceContext;
-    D3D_FEATURE_LEVEL d3dFeatureLevel;
     if (SUCCEEDED(hr))
     {
         hr = D3D11CreateDevice(
@@ -80,65 +110,21 @@ int main()
             &spD3DDevice,
             &d3dFeatureLevel,
             &spD3DDeviceContext);
-    }
+    }*/
 
     CComPtr<IDXGIDevice> spDxgiDevice;
     if (SUCCEEDED(hr))
     {
         hr = spD3DDevice->QueryInterface(&spDxgiDevice);
     }
-
-    /*
-    UINT dwSupport_DXGI_FORMAT_R8G8B8A8_UNORM;
-    if (SUCCEEDED(hr))
-    {
-        hr = spD3DDevice->CheckFormatSupport(
-            DXGI_FORMAT_R8G8B8A8_UNORM,
-            &dwSupport_DXGI_FORMAT_R8G8B8A8_UNORM);
-    }
-
-    UINT dwSupport_DXGI_FORMAT_B8G8R8A8_UNORM;
-    if (SUCCEEDED(hr))
-    {
-        hr = spD3DDevice->CheckFormatSupport(
-            DXGI_FORMAT_B8G8R8A8_UNORM,
-            &dwSupport_DXGI_FORMAT_B8G8R8A8_UNORM);
-    }*/
     
-    /*
-    CComPtr<ID3D10Texture2D> spTexture;
+    CComPtr<ID2D1Device> spD2DDevice;
     if (SUCCEEDED(hr))
     {
-        // https://msdn.microsoft.com/en-us/library/windows/desktop/dd370966(v=vs.85).aspx
-
-        D3D10_TEXTURE2D_DESC desc;
-        ZeroMemory(&desc, sizeof(desc));
-
-        desc.Width = 128;
-        desc.Height = 128;
-
-        desc.ArraySize = 1;
-        desc.BindFlags = D3D10_BIND_RENDER_TARGET;
-        desc.CPUAccessFlags = D3D10_CPU_ACCESS_READ | D3D10_CPU_ACCESS_WRITE;
-        desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-        desc.MipLevels = 1;
-        desc.MiscFlags = 0;
-        desc.SampleDesc.Count = 1;
-        desc.SampleDesc.Quality = 0;
-        desc.Usage = D3D10_USAGE_DEFAULT;
-
-        hr = spD3DDevice->CreateTexture2D(
-            &desc,
-            NULL,
-            &spTexture);
+        hr = spD2DFactory->CreateDevice(
+            spDxgiDevice,
+            &spD2DDevice);
     }
-
-    CComPtr<IDXGISurface> spSurface;
-    if (SUCCEEDED(hr))
-    {
-        hr = spTexture->QueryInterface(&spSurface);
-    }
-    */
 
     CComPtr<IWICBitmap> spBitmap;
     if (SUCCEEDED(hr))
@@ -147,7 +133,7 @@ int main()
             1024,
             1024,
             GUID_WICPixelFormat32bppPBGRA,
-            WICBitmapCacheOnLoad,
+            WICBitmapCacheOnDemand,
             &spBitmap);
     }
 
@@ -184,30 +170,35 @@ int main()
         hr = spBitmapLock->GetStride(&dwBitmapStride);
     }
 
-    CComPtr<ID3D11Texture2D> spSharedTexture;
+    // Try to share bitmap as a texture and then map the texture?
+    CComPtr<ID3D10Texture2D> spSharedTexture;
     if (SUCCEEDED(hr))
     {
-        D3D11_TEXTURE2D_DESC desc;
+        D3D10_TEXTURE2D_DESC desc;
         ZeroMemory(&desc, sizeof(desc));
 
         desc.Width = dwBitmapWidth;
         desc.Height = dwBitmapHeight;
-        desc.ArraySize = 1;
-        desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-        desc.CPUAccessFlags = 0; // D3D10_CPU_ACCESS_READ | D3D10_CPU_ACCESS_WRITE;
-        desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
         desc.MipLevels = 1;
-        desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
+        desc.ArraySize = 1;
+        desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
         desc.SampleDesc.Count = 1;
         desc.SampleDesc.Quality = 0;
-        desc.Usage = D3D11_USAGE_DEFAULT;
+        desc.Usage = D3D10_USAGE_DEFAULT;
+        desc.BindFlags = D3D10_BIND_RENDER_TARGET | D3D10_BIND_SHADER_RESOURCE;
+        desc.CPUAccessFlags = D3D10_CPU_ACCESS_READ | D3D10_CPU_ACCESS_WRITE;
+        desc.MiscFlags = 0
+            | D3D10_RESOURCE_MISC_SHARED
+            //| D3D10_RESOURCE_MISC_SHARED_NTHANDLE
+            //| D3D10_RESOURCE_MISC_SHARED_KEYEDMUTEX
+            ;
 
-        D3D11_SUBRESOURCE_DATA subData;
+        D3D10_SUBRESOURCE_DATA subData;
         ZeroMemory(&subData, sizeof(subData));
 
         subData.pSysMem = pBitmapData;
         subData.SysMemPitch = dwBitmapStride;
-        subData.SysMemSlicePitch = dwBitmapStride * dwBitmapHeight; //cbBitmap;
+        subData.SysMemSlicePitch = dwBitmapStride * dwBitmapHeight;
 
         hr = spD3DDevice->CreateTexture2D(
             &desc,
@@ -215,59 +206,29 @@ int main()
             &spSharedTexture);
     }
 
-    /*
-    D3D11_MAPPED_TEXTURE2D mapped;
-    ZeroMemory(&mapped, sizeof(mapped));
-    if (SUCCEEDED(hr))
-    {
-        hr = spSharedTexture->Map(
-            D3D10CalcSubresource(0, 0, 1),
-            D3D10_MAP_READ_WRITE,
-            0,
-            &mapped);
-    }
-
-    if (SUCCEEDED(hr))
-    {
-        spSharedTexture->Unmap(
-            D3D10CalcSubresource(0, 0, 1));
-    }*/
-
     CComPtr<IDXGISurface> spSharedDxgiSurface;
     if (SUCCEEDED(hr))
     {
         hr = spSharedTexture->QueryInterface(&spSharedDxgiSurface);
     }
 
-    /*
-    DXGI_MAPPED_RECT mappedRect;
-    ZeroMemory(&mappedRect, sizeof(mappedRect));
+    CComPtr<IDXGIResource> spDxgiResource;
     if (SUCCEEDED(hr))
     {
-        hr = spSharedDxgiSurface->Map(
-            &mappedRect,
-            DXGI_MAP_DISCARD | DXGI_MAP_WRITE);
+        hr = spSharedTexture->QueryInterface(&spDxgiResource);
     }
 
+    /*CComPtr<IDXGIKeyedMutex> spSurfaceLock;
     if (SUCCEEDED(hr))
     {
-        hr = spSharedDxgiSurface->Unmap();
-    }
-    */
+        hr = spDxgiResource->QueryInterface(&spSurfaceLock);
+    }*/
 
-    CComPtr<ID2D1Device1> spD2DDevice;
-    if (SUCCEEDED(hr))
-    {
-        hr = spD2DFactory->CreateDevice(
-            spDxgiDevice,
-            &spD2DDevice);
-    }
-
-    CComPtr<ID2D1DeviceContext1> spD2DDeviceContext;
+    /*CComPtr<ID2D1DeviceContext> spD2DDeviceContext;
     if (SUCCEEDED(hr))
     {
         hr = spD2DDevice->CreateDeviceContext(
-            D2D1_DEVICE_CONTEXT_OPTIONS_NONE,
+            D2D1_DEVICE_CONTEXT_OPTIONS_ENABLE_MULTITHREADED_OPTIMIZATIONS,
             &spD2DDeviceContext);
     }
 
@@ -277,6 +238,8 @@ int main()
         D2D1_BITMAP_PROPERTIES1 bitmapProps;
         ZeroMemory(&bitmapProps, sizeof(bitmapProps));
 
+        bitmapProps.dpiX = 96;
+        bitmapProps.dpiY = 96;
         bitmapProps.bitmapOptions = D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW;
         bitmapProps.pixelFormat.alphaMode = D2D1_ALPHA_MODE_PREMULTIPLIED;
         bitmapProps.pixelFormat.format = DXGI_FORMAT_UNKNOWN;
@@ -287,24 +250,28 @@ int main()
             &spD2DBitmap);
     }
 
-    /*
-    D2D1_MAPPED_RECT mappedRect;
-    ZeroMemory(&mappedRect, sizeof(mappedRect));
-    if (SUCCEEDED(hr))
-    {
-        hr = spD2DBitmap->Map(
-            D2D1_MAP_OPTIONS_READ,
-            &mappedRect);
-    }
-
-    if (SUCCEEDED(hr))
-    {
-        hr = spD2DBitmap->Unmap();
-    }*/
-
     if (SUCCEEDED(hr))
     {
         spD2DDeviceContext->SetTarget(spD2DBitmap);
+    }*/
+
+    CComPtr<ID2D1RenderTarget> spRenderTarget;
+    if (SUCCEEDED(hr))
+    {
+        hr = spD2DFactory->CreateDxgiSurfaceRenderTarget(
+            spSharedDxgiSurface,
+            D2D1::RenderTargetProperties(
+                D2D1_RENDER_TARGET_TYPE_DEFAULT,
+                D2D1::PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED),
+                96,
+                96),
+            &spRenderTarget);
+    }
+
+    CComPtr<ID2D1DeviceContext> spD2DDeviceContext;
+    if (SUCCEEDED(hr))
+    {
+        hr = spRenderTarget->QueryInterface(&spD2DDeviceContext);
     }
 
     D2D1_SIZE_U rtSize;
@@ -330,21 +297,29 @@ int main()
             &spBrush);
     }
 
-    D2D1_TAG tag1;
-    D2D1_TAG tag2;
+    /*if (SUCCEEDED(hr)) 
+    {
+        hr = spSurfaceLock->AcquireSync(0, INFINITE);
+    }*/
+
     if (SUCCEEDED(hr))
     {
         spD2DDeviceContext->BeginDraw();
-        
-        D2D1_COLOR_F color;
-        ZeroMemory(&color, sizeof(color));
 
+        spD2DDeviceContext->SetPrimitiveBlend(D2D1_PRIMITIVE_BLEND_COPY);
+
+        D2D1_COLOR_F color = D2D1::ColorF(D2D1::ColorF::Blue);
+        //ZeroMemory(&color, sizeof(color));
+
+        /*
         color.a = 1.0f;
         color.b = 1.0f;
         color.g = 1.0f;
-        color.r = 1.0f;
+        color.r = 1.0f;*/
+
+
         spD2DDeviceContext->Clear(color);
-        spD2DDeviceContext->Flush(&tag1, &tag2);
+        //spD2DDeviceContext->Flush(&tag1, &tag2);
 
         D2D1_ELLIPSE ellipse;
         ZeroMemory(&ellipse, sizeof(ellipse));
@@ -357,20 +332,67 @@ int main()
         spD2DDeviceContext->FillEllipse(
             ellipse,
             spBrush);
-        spD2DDeviceContext->Flush(&tag1, &tag2);
+    }
 
+    if (SUCCEEDED(hr))
+    {
+        hr = spD2DDeviceContext->Flush();
+    }
+
+    D2D1_TAG tag1;
+    D2D1_TAG tag2;
+    if (SUCCEEDED(hr))
+    {
         ZeroMemory(&tag1, sizeof(tag1));
         ZeroMemory(&tag2, sizeof(tag2));
         hr = spD2DDeviceContext->EndDraw(&tag1, &tag2);
     }
 
+    /*
+    if (SUCCEEDED(hr))
+    {
+        spD2DLock->Enter();
+        spD2DLock->Leave();
+    }
+    */
+
+    /*if (SUCCEEDED(hr))
+    {
+        hr = spSurfaceLock->ReleaseSync(0);
+    }*/
+
+    ::Sleep(2000);
+
+    /*
+    if (SUCCEEDED(hr))
+    {
+        hr = spSurfaceLock->AcquireSync(0, INFINITE);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        spD3DDeviceContext->Flush();
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = spSurfaceLock->ReleaseSync(0);
+    }
+
+    ::Sleep(2000);
+    */
+
     if (SUCCEEDED(hr))
     {
         spBrush.Release();
-        spD2DDeviceContext->SetTarget(NULL);
-        spD2DBitmap.Release();
+        //spD2DDeviceContext->SetTarget(NULL);
+        //spD2DBitmap.Release();
+        //spD2DDeviceContext.Release();
+        //spD2DDevice.Release();
         spD2DDeviceContext.Release();
         spD2DDevice.Release();
+        //spD3DDeviceContext.Release();
+        spD3DDevice.Release();
         spSharedDxgiSurface.Release();
         spSharedTexture.Release();
         spBitmapLock.Release();
